5f007779ea683f5b5f8c90557a655e75a50b6682
egorka <egorka_988@mail.ru>
2025-01-21 16:44:25 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="app">
+    <form>
+      <h4>Создание поста</h4>
+      <input
+        v-bind:value="title"
+        class="input"
+        type="text"
+        placeholder="Название поста"
+      />
+      <input
+        v-bind:value="body"
+        class="input"
+        type="text"
+        placeholder="Описание поста"
+      />
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "Пост о JS",
+          body: "JavaScript",
+        },
+        {
+          id: 2,
+          title: "Пост о JS 1",
+          body: "JavaScript 1",
+        },
+        {
+          id: 3,
+          title: "Пост о JS 2",
+          body: "JavaScript 2",
+        },
+        {
+          id: 4,
+          title: "Пост о JS 3",
+          body: "JavaScript 3",
+        },
+      ],
+      title: "",
+      body: "",
+    };
+  },
+
+  methods: {
+    createPost() {
+
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
